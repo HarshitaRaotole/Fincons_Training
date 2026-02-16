7362cba4351ccd56a5f66062a6937e6ba99106f3
HarshitaRaotole <2024.harshita.raotole@ves.ac.in>
2026-02-16 16:27:24 +0530

    Added Angular Material dependency and configuration

--- W4_Project/parking-ui/src/app/app.module.ts
+++ W4_Project/parking-ui/src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+// ⭐ NEW: Required for Angular Material Animations (Dialogs, Inputs, Snackbars)
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Page Components
 import { ParkingLotsComponent } from './components/parking-lots/parking-lots.component';
 import { VehicleEntryComponent } from './components/vehicle-entry/vehicle-entry.component';
 import { ActiveSessionsComponent } from './components/active-sessions/active-sessions.component';
 import { VehicleExitComponent } from './components/vehicle-exit/vehicle-exit.component';
 import { ParkingHistoryComponent } from './components/parking-history/parking-history.component';
+
+// Dialog Components
+import { TicketDialogComponent } from './components/ticket-dialog/ticket-dialog.component';
+import { ReceiptDialogComponent } from './components/receipt-dialog/receipt-dialog.component';
+
+// Angular Material Imports
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -18,13 +40,30 @@ import { ParkingHistoryComponent } from './components/parking-history/parking-hi
     VehicleEntryComponent,
     ActiveSessionsComponent,
     VehicleExitComponent,
-    ParkingHistoryComponent
+    ParkingHistoryComponent,
+    TicketDialogComponent,
+    ReceiptDialogComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // ⭐ Critical: Added this
     AppRoutingModule,
-    HttpClientModule, // Needed for API calls
-    FormsModule       // Needed for Forms
+    HttpClientModule,
+    FormsModule,
+    
+    // Material Modules
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSelectModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
